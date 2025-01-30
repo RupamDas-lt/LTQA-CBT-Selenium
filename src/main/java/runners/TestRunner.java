5944d44afa83bcb955154ffa265ef810ecc53497
rupamd <rupamd@lambdatest.com>
2025-01-30 20:02:10 +0530

    Added support for report portal

--- src/main/java/runners/TestRunner.java
+++ src/main/java/runners/TestRunner.java
@@ -2,14 +2,20 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import utility.ScenarioUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import static java.lang.System.getProperty;
 import static java.lang.System.setProperty;
 
 @CucumberOptions(features = { "src/test/features" }, glue = { "stepDefinitions", "Hooks" }, plugin = { "pretty",
-  "rerun:rerun/failed_scenarios.txt", "json:target/cucumber-reports/CucumberTestReport.json" })
+  "rerun:rerun/failed_scenarios.txt", "json:target/cucumber-reports/CucumberTestReport.json",
+  "com.epam.reportportal.cucumber.ScenarioReporter" })
 
 public class TestRunner extends AbstractTestNGCucumberTests {
   public TestRunner() {
@@ -25,5 +31,20 @@ public class TestRunner extends AbstractTestNGCucumberTests {
       return ScenarioUtils.repeatScenarios(super.scenarios(), runNTimes);
     }
     return super.scenarios();
+  }
+
+  @BeforeSuite
+  public void initializeReportPortalParameters() {
+    try (InputStream input = TestRunner.class.getClassLoader().getResourceAsStream("reportportal.properties")) {
+      if (input == null) {
+        System.err.println("Sorry, unable to find reportportal.properties");
+        return;
+      }
+      Properties prop = new Properties();
+      prop.load(input);
+      prop.forEach((key, value) -> System.setProperty(key.toString(), value.toString()));
+    } catch (IOException ex) {
+      System.err.println("Unable to set reportportal properties: " + ex);
+    }
   }
 }
