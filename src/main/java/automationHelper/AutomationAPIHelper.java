4ac97548178b63eace35227468c35945e44d4dc8
rupamd <rupamd@lambdatest.com>
2025-02-05 16:22:18 +0530

    Small change

--- src/main/java/automationHelper/AutomationAPIHelper.java
+++ src/main/java/automationHelper/AutomationAPIHelper.java
@@ -1,5 +1,6 @@
 package automationHelper;
 
+import TestManagers.ApiManager;
 import io.restassured.response.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
