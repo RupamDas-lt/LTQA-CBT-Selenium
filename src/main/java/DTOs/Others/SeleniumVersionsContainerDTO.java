88dbaae0b9dccdd0f9606656c64136789a336f97
rupamd <rupamd@lambdatest.com>
2025-03-17 13:39:41 +0530

    Removed unwanted file

--- src/main/java/DTOs/Others/SeleniumVersionsContainerDTO.java
+++
@@ -1,9 +0,0 @@
-package DTOs.Others;
-
-import lombok.Data;
-
-import java.util.ArrayList;
-
-@Data public class SeleniumVersionsContainerDTO {
-  private ArrayList<SeleniumVersionsDTO> container;
-}
