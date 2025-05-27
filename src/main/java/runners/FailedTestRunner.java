bf03ad5f19538aa997664852da89d1ebb9d3bcb0
rupamd <rupamd@lambdatest.com>
2025-05-28 01:32:34 +0530

    Updated FailedTestRunner.java

--- src/main/java/runners/FailedTestRunner.java
+++ src/main/java/runners/FailedTestRunner.java
@@ -1,13 +1,19 @@
 package runners;
 
+import automationHelper.AutomationAPIHelper;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import utility.ScenarioUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static java.lang.System.getProperty;
 import static java.lang.System.setProperty;
-import static utility.FrameworkConstants.TEST_ATTEMPT;
+import static utility.FrameworkConstants.*;
 
 @CucumberOptions(features = { "@rerun/failed_scenarios.txt" }, glue = { "stepDefinitions",
   "Hooks" }, monochrome = true, plugin = { "pretty", "rerun:rerunFailedFirstAttempt/failed_scenarios_post_rerun.txt",
@@ -28,5 +34,40 @@ import static utility.FrameworkConstants.TEST_ATTEMPT;
       return ScenarioUtils.repeatScenarios(super.scenarios(), runNTimes);
     }
     return super.scenarios();
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+
+    if (System.getProperty(PUSH_LOGS_TO_REPORT_PORTAL, "false").equalsIgnoreCase("true")) {
+      initializeReportPortalParameters();
+    }
+
+    if (getProperty(TEST_PREREQUISITES, "true").equals("true"))
+      setPrerequisite();
+  }
+
+  private void initializeReportPortalParameters() {
+    try (InputStream input = TestRunner.class.getClassLoader().getResourceAsStream("reportportal.properties")) {
+      if (input == null) {
+        System.err.println("Sorry, unable to find reportportal.properties");
+        return;
+      }
+      Properties prop = new Properties();
+      prop.load(input);
+      prop.forEach((key, value) -> {
+        /// If we are passing any property from CLI, then we will not override it
+        if (System.getProperty(key.toString(), "").isEmpty()) {
+          System.setProperty(key.toString(), value.toString());
+        }
+      });
+    } catch (IOException ex) {
+      System.err.println("Unable to set reportportal properties: " + ex);
+    }
+  }
+
+  private void setPrerequisite() {
+    AutomationAPIHelper automationAPIHelper = new AutomationAPIHelper();
+    automationAPIHelper.fetchAllGeoLocationsFromCapsGeneratorAndStoreInJsonFile();
   }
 }
